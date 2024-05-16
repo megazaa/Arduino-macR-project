cdeedecba5eb37e1ac2e6e3f3e5518a33176cdb3
mega <megaza28@gmail.com>
2024-05-17 03:46:45 +0700

    Create webserverEsp32_GPT_JSON.ino

---
+++ webserverEsp32_GPT_JSON/webserverEsp32_GPT_JSON.ino
@@ -0,0 +1,69 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#include <ArduinoJson.h>
+
+const char* ssid = "Mega Who_plus";
+const char* password = "23571234";
+ESP8266WebServer server(80);
+
+void handleRoot() {
+  server.send(200, "text/plain", "ESP8266 Web Server");
+}
+
+void handleNotFound() {
+  server.send(404, "text/plain", "Not found");
+}
+
+void handlePost() {
+  if (server.hasArg("plain") == false) {
+    server.send(400, "text/plain", "Body not received");
+    return;
+  }
+
+  String requestBody = server.arg("plain");
+  StaticJsonDocument<200> doc;
+  DeserializationError error = deserializeJson(doc, requestBody);
+
+  if (error) {
+    server.send(400, "text/plain", "Invalid JSON");
+    return;
+  }
+
+  int x = doc["x"];
+  int y = doc["y"];
+
+  // Print the values to serial monitor
+  Serial.print("(");
+  Serial.print(x);
+  Serial.print(",");
+  Serial.print(y);
+  Serial.println(")");
+
+  // Send response
+  server.send(200, "application/json", "{\"status\":\"received\"}");
+
+  // Forward data to Arduino Nano
+  //Serial.print(x);
+  //Serial.print(",");
+  //Serial.println(y);
+}
+
+void setup() {
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
+
+  Serial.println("Connected to WiFi");
+  server.on("/", handleRoot);
+  server.onNotFound(handleNotFound);
+  server.on("/post", HTTP_POST, handlePost);
+  server.begin();
+}
+
+void loop() {
+  server.handleClient();
+}
